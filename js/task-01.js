6fd82b2841423da8660648b33098ec6a0249c7ae
Ihor <52411206+Ihor-ok@users.noreply.github.com>
2022-02-23 20:54:12 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,23 +1,10 @@
 const numberOfCategories = document.querySelectorAll("li.item").length;
 console.log(`Number of categories: ${numberOfCategories}`);
 
-const firstTitle = document.querySelector(".item:first-child h2").textContent;
-console.log(`Category: ${firstTitle}`);
-const numberFirstTitle = document.querySelectorAll(
-  ".item:first-child li"
-).length;
-console.log(`Elements: ${numberFirstTitle}`);
+const arrItem = [...document.querySelectorAll("li.item")];
+const arrlist = [...document.querySelectorAll("li.item ul")];
 
-const averageTitle = document.querySelector(
-  ".item:nth-child(2) h2"
-).textContent;
-console.log(`Category: ${averageTitle}`);
-const numberAverageTitle = document.querySelectorAll(
-  ".item:nth-child(2) li"
-).length;
-console.log(`Elements: ${numberAverageTitle}`);
-
-const lastTitle = document.querySelector(".item:last-child h2").textContent;
-console.log(`Category: ${lastTitle}`);
-const numberLastTitle = document.querySelectorAll(".item:last-child li").length;
-console.log(`Elements: ${numberLastTitle}`);
+arrItem.forEach((item) => {
+  console.log(`Category: ${item.querySelector("h2").textContent}`);
+  console.log(`Elements: ${item.querySelectorAll("li").length}`);
+});
