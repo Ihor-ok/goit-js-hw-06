526b5222a576d18c03406c9f65879b350fdb393d
Ihor <52411206+Ihor-ok@users.noreply.github.com>
2022-02-24 00:12:25 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -8,43 +8,15 @@ const ingredients = [
 ];
 
 const list = document.querySelector("#ingredients");
+const arrIngredients = [];
 
-const ingredientPotatoes = document.createElement("li");
-ingredientPotatoes.classList.add("item");
-ingredientPotatoes.textContent = "Potatoes";
+for (let i = 0; i < ingredients.length; i++) {
+  const ingredient = document.createElement("li");
+  ingredient.classList.add("item");
+  ingredient.textContent = ingredients[i];
 
-const ingredientMushrooms = document.createElement("li");
-ingredientMushrooms.classList.add("item");
-ingredientMushrooms.textContent = "Mushrooms";
-
-const ingredientGarlic = document.createElement("li");
-ingredientGarlic.classList.add("item");
-ingredientGarlic.textContent = "Garlic";
-
-const ingredientTomatos = document.createElement("li");
-ingredientTomatos.classList.add("item");
-ingredientTomatos.textContent = "Tomatos";
-
-const ingredientHerbs = document.createElement("li");
-ingredientHerbs.classList.add("item");
-ingredientHerbs.textContent = "Herbs";
-
-const ingredientCondiments = document.createElement("li");
-ingredientCondiments.classList.add("item");
-ingredientCondiments.textContent = "Condiments";
-
-list.prepend(
-  ingredientPotatoes,
-  ingredientMushrooms,
-  ingredientGarlic,
-  ingredientTomatos,
-  ingredientHerbs,
-  ingredientCondiments
-);
-
-console.log(ingredientPotatoes);
-console.log(ingredientMushrooms);
-console.log(ingredientGarlic);
-console.log(ingredientTomatos);
-console.log(ingredientHerbs);
-console.log(ingredientCondiments);
+  arrIngredients.push(ingredient);
+  console.log(ingredient);
+}
+console.log(arrIngredients);
+list.prepend(...arrIngredients);
