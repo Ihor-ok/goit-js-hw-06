16cc5e87206f0e98f5c2982baefd90b0a0ec9beb
Ihor <kovaluk_igor@ukr.net>
2023-03-29 21:53:29 +0300

    done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refBody = document.querySelector('body')
+const refBackgroundColorText = document.querySelector('.color');
+const refButton = document.querySelector('.change-color');
+
+refButton.addEventListener('click', chengeColor);
+
+function chengeColor(event) {
+
+  const color = getRandomHexColor();
+
+  refBackgroundColorText.textContent = color
+  refBody.style.backgroundColor = color;
+};
