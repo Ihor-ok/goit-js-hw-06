5f2bf05563794911ac7a1944ab34c1d0cae8f089
Ihor <kovaluk_igor@ukr.net>
2023-03-29 21:21:10 +0300

    done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const data = {};
+
+const refForm = document.querySelector('.login-form');
+
+refForm.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+    event.preventDefault()
+
+    data.email = event.currentTarget.elements.email.value;
+    data.password = event.currentTarget.elements.password.value;
+
+    if (event.currentTarget.elements.email.value.length === 0 || event.currentTarget.elements.password.value.length === 0) {
+        alert("Всі поля повинні бути заповнені");
+        return;
+
+    };
+
+    refForm.reset();
+
+    console.log(data);
+
+};
 
